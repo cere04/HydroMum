e9762b12531b9f8f5cc42ea8c62b15e3839b6097
cere04 <nicolacesa24@gmail.com>
2022-12-19 22:27:43 +0100

    Progetto Rotary

---
+++ inc/footer.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="container" id="bottom">
+  <div class="row">
+    <div class="col-sm-12 col-md-6">
+      <h6>About Us</h6>
+      <p class="text-justify">Siamo 5 ragazzi proveniente dalle zone del Maceratese e del Fermano, frequentiamo il 5 anno dell’indirizzo di informatica dell’istituto tecnico G.M.Montani di Fermo; abbiamo assunto i seguenti ruoli all’interno del progetto:</p>
+        <br><br>
+        <a href="https://www.instagram.com/leonardomobbili/"><i class="fa fa-instagram" aria-hidden="true"></i> &nbsp;&nbsp;</a> <a href="https://www.instagram.com/leonardomobbili/">Leonardo Mobbili</a>
+        <br>
+        <a href="https://www.instagram.com/_rotuale_/"><i class="fa fa-instagram" aria-hidden="true"></i> &nbsp;&nbsp;</a> <a href="https://www.instagram.com/_rotuale_/">Alessandro Rotaru</a> 
+        <br>
+        <a href="https://www.instagram.com/_laauraa.a_/"><i class="fa fa-instagram" aria-hidden="true"></i> &nbsp;&nbsp;</a> <a href="https://www.instagram.com/_laauraa.a_/">Laura Andrenacci</a>
+        <br>
+        <a href="https://www.instagram.com/marco.mangiapanini/"><i class="fa fa-instagram" aria-hidden="true"></i> &nbsp;&nbsp;</a> <a href="https://www.instagram.com/marco.mangiapanini/">Marco Pagnanini </a> 
+        <br>
+        <a href="https://www.instagram.com/_nicola.cere/"><i class="fa fa-instagram" aria-hidden="true"></i> &nbsp;&nbsp;</a> <a href="https://www.instagram.com/_nicola.cere/">Nicola Cesaretti </a> 
+      
+    </div>
+  </div>
+  <hr class="small">
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-sm-6 col-12">
+      <p class="copyright-text">Copyright © 2022 All Rights Reserved by HydroMum
+      </p>
+    </div>
+    <div class="spacer"></div>
+  </div>
+</div>
